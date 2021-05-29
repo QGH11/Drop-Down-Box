7d0358ced97a5b8fd85905cb1f16ecff8e40cc8e
QGH <Guanhua.Qiao@outlook.com>
2021-05-28 22:48:32 -0700

    finished

--- select.js
+++ select.js
@@ -3,9 +3,12 @@ export default class Select {
     constructor(element) {
         this.element = element
         this.options = getFormattedOptions(element.querySelectorAll("option"))
+
+        //creates div sapn ul
         this.customElement = document.createElement("div")
         this.labelElement = document.createElement("span")
         this.optionsCustomElement = document.createElement("ul")
+        
         setupCustomElement(this)
         element.style.display = "none"
         element.after(this.customElement)
